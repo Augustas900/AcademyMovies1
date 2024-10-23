9bb707fa463db4a16848b82c4bf3398d4e0368f5
Augustas900 <timofejevas.augustinas@gmail.com>
2024-10-23 11:03:30 +0300

    Update MovieController.java

--- src/main/java/lt/ca/javau10/MovieController.java
+++ src/main/java/lt/ca/javau10/MovieController.java
@@ -9,21 +9,28 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-
-
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
     @Autowired
     private MovieService service;
 
+    // GET endpoint to retrieve all movies
     @GetMapping
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
     }
 
+    // GET endpoint to retrieve a single movie by IMDb ID
     @GetMapping("/{imdbId}")
-    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
         return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+
+    // POST endpoint to create a new movie
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie createdMovie = service.saveMovie(movie); // Assuming a saveMovie method exists in the service
+        return new ResponseEntity<>(createdMovie, HttpStatus.CREATED);
     }
 }
